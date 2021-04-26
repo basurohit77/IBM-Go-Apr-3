16964e890d072f570a0d2610b309572811983883
Rohit Basu <basurohit77@gmail.com>
2021-04-26 19:39:55 +0530

    Update functiontype.go

--- 04-functions/functiontype.go
+++ 04-functions/functiontype.go
@@ -32,6 +32,11 @@ func main() {
 	//fmt.Println(getRand(200))
 	v1, v2 := getRand(1000)
 	fmt.Println(v1, v2)
+        
+       // operation returns 5, 10 to getRand, and then getRand returns rand1, rand2 to log function
+        v3, v4 := log(5, 10, getRand)
+	fmt.Println(v3, v4)
+
 }
 
 /*
@@ -41,6 +46,11 @@ func greet(firstname string, lastname string) string {
 }
 */
 
+// Functions can be passed as arguments to other functions
+func log(m1, m2 int, operation func(int, int) (int, int) ) (int, int) {
+	fmt.Println("process m1 and m2")
+	return operation(m1, m2)
+}
 
 func getRand(limit int) (rand1 int, rand2 int) {
 	rand1 = rand.Intn(limit)
