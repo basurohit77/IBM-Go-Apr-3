735c1fcc0f772811d6c21103a5cb09b93999175c
Rohit Basu <basurohit77@gmail.com>
2021-04-26 19:48:42 +0530

    Update closure.go

--- 04-functions/closure.go
+++ 04-functions/closure.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+        fmt.Println("from main")
 	count := getCounter()
 	fmt.Println(count())
 	fmt.Println(count())
@@ -16,11 +17,12 @@ func main() {
 func main2() {
 	fmt.Println("from main2")
 	var count = getCounter()
-	fmt.Println(count())
+	fmt.Println(count()) // counter will be 7
 	fmt.Println(count())
 	fmt.Println(count())
 }
-
+// closure is something a outer function cantain a variable, tha t variable is process in inner function and returned
+// Thus the inner function keeps the variable alive of the outer function.
 func getCounter() func() int {
 	var counter int = 0
 	return func() int {
