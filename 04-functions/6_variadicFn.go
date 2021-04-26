9802353fcf9ce3055b17b3f99e7926c2e006861d
Rohit Basu <basurohit77@gmail.com>
2021-04-26 20:22:35 +0530

    Update and rename variadicFn.go to 6_variadicFn.go

--- 04-functions/6_variadicFn.go
+++ 04-functions/6_variadicFn.go
@@ -8,6 +8,7 @@ func main() {
 	fmt.Println(add(10, 20, 30, 40, 50))
 }
 
+// Dynamic number of argument is known ar variadic function
 func add(nos ...int) int {
 	sum := 0
 	for i := 0; i < len(nos); i++ {
